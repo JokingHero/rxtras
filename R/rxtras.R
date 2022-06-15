f4b06a88d914a12898e5cff89e7f357868d1054a
JokingHero <kornel.labun@gmail.com>
2022-06-15 17:30:22 +0200

    ggplot2 settings on

--- R/rxtras.R
+++ R/rxtras.R
@@ -8,7 +8,18 @@
 #'
 "_PACKAGE"
 .onAttach <- function(libname, pkgname) {
+  ggplot2_settings_on()
+}
+
+
+#' @title Set options for ggplot2
+#'
+#' @description Set theme to theme science and colors and fill.
+#' @export
+#'
+ggplot2_settings_on <- function() {
   theme_set(theme_science())
   options(ggplot2.discrete.fill = color_scheme)
   options(ggplot2.discrete.color = color_scheme)
+  return(invisible())
 }
