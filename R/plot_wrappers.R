6d0e3b118a24c7749fb16fc29b25b43d72548e65
JokingHero <kornel.labun@gmail.com>
2026-01-16 20:15:42 +0100

    fix some deprecation warnings

--- R/plot_wrappers.R
+++ R/plot_wrappers.R
@@ -42,6 +42,5 @@ as.pngpdf <- function(filename, plot = ggplot2::last_plot(), scale = 1,
   ggsave(paste0(filename, ".png"), plot = plot, scale = scale, width = width,
          height = height, units = units, dpi = dpi, limitsize = FALSE)
   ggsave(paste0(filename, ".pdf"), plot = plot, scale = scale, width = width,
-         height = height, units = units, dpi = dpi, limitsize = FALSE,
-         device = grDevices::cairo_pdf)
+         height = height, units = units, dpi = dpi, limitsize = FALSE)
 }
